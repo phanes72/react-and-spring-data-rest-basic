449da81b9f7e190e606e1a56c46c73c7196ad5dc
Patrick Hanes <phanes72@gmail.com>
2023-03-21 06:11:59 -0600

    Removed stuff didn't need from when debugging eariler

--- react-and-spring-data-rest-basic/src/main/java/com/pjhanes/reactandspringdatarestbasic/payroll/ReactAndSpringDataRestBasicApplication.java
+++ react-and-spring-data-rest-basic/src/main/java/com/pjhanes/reactandspringdatarestbasic/payroll/ReactAndSpringDataRestBasicApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"com.pjhanes.reactandspringdatarestbasic.payroll"})
-@EntityScan(basePackages = {"com.pjhanes.reactandspringdatarestbasic.payroll"})    
 public class ReactAndSpringDataRestBasicApplication {
 
 	public static void main(String[] args) {
--- react-and-spring-data-rest-basic/src/test/java/com/pjhanes/reactandspringdatarestbasic/ReactAndSpringDataRestBasicApplicationTests.java
+++ react-and-spring-data-rest-basic/src/test/java/com/pjhanes/reactandspringdatarestbasic/ReactAndSpringDataRestBasicApplicationTests.java
@@ -3,10 +3,10 @@ package com.pjhanes.reactandspringdatarestbasic;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ReactAndSpringDataRestBasicApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
