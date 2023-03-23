234fc04dc631a13a6596d8ec9a31f700522ff390
Patrick Hanes <phanes72@gmail.com>
2023-03-23 06:31:31 -0600

    Cleaned up a small error issue in the pom

--- react-and-spring-data-rest-basic/src/main/java/com/pjhanes/reactandspringdatarestbasic/payroll/ReactAndSpringDataRestBasicApplication.java
+++ react-and-spring-data-rest-basic/src/main/java/com/pjhanes/reactandspringdatarestbasic/payroll/ReactAndSpringDataRestBasicApplication.java
@@ -2,8 +2,7 @@ package com.pjhanes.reactandspringdatarestbasic.payroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
 public class ReactAndSpringDataRestBasicApplication {
